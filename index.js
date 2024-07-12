428b03434c89e9a66ea50d39f57470f3a743ff41
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-07-12 11:41:34 +0100

    it connects to notion

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// EMAIL AND SERVER
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
-// Configurar o body-parser para processar requisições JSON
 app.use(bodyParser.json());
+require('dotenv').config();
+
+// NOTION
+const { Client } = require('@notionhq/client');
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 // Rota para receber webhooks de e-mail
 app.post('/email', (req, res) => {
@@ -13,12 +17,33 @@ app.post('/email', (req, res) => {
   // Faça alguma ação com o e-mail recebido
   console.log('Novo e-mail recebido:', emailData);
 
-  // Enviar uma resposta ao serviço de e-mail
-  res.status(200).send('E-mail recebido com sucesso!');
+  // Month - Day - Year  Hour : Minute
+  const event = new Date('12-07-2024 14:48');
+
+  (async () => {
+    const response = await notion.pages.create({
+      "parent": {
+        "type": "database_id",
+        "database_id": "7107291622514df2ac798e53e3291541"
+      },
+      "properties": {
+          "Reserva": {
+            "type": "title",
+            "title": [{ "type": "text", "text": { "content": "#2412" } }]
+          },
+          "Data": {
+            "type": "date",
+            "date": { "start": event.toISOString() }
+          }
+      }
+  });
+    console.log(response);
+  })();
+
+  res.status(200).send('E-mail recebido com sucesso!');  
 });
 
-// Iniciar o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
